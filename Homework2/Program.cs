9717aed86e59edda773fcf3f3bf41decd9e77b34
reaper <reaper>
2022-10-12 22:17:45 +0300

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите рассматриваемое число");
+int n = int.Parse(Console.ReadLine());
+
+void GetSum(int n)
+{
+    int sum = 0;
+    int length = 0;
+    int f = 0;
+    while (n > 0)
+    {
+        length = length + 1;
+        f = n % 10;
+        n = n / 10;
+        sum =  sum + f;
+    }
+    Console.WriteLine("Сумма цифр в числе = " + sum);
+}
+
+
+
+GetSum(n);
